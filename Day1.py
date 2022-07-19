d55323418b4de55653f1647e80b6372fa111a60e
Aditya Danturthi <aditya2096@gmail.com>
2022-07-18 22:08:21 -0230

    mathematical operations and datatypes

---
+++ Day1.py
@@ -0,0 +1,18 @@
+# Day1: 
+print(2+3) # addition
+print(3-1) # subtraction
+print(2*3) # multiplication
+print(3/2) # division
+print(3**2) # exponential
+print(3%2) # modulus
+print(3//2) # floor division 
+
+# Checking datatypes
+print(type(10)) # int
+print(type(3.14)) # float
+print(type(1+3j)) # complex
+print(type('Aeiou')) # string
+print(type([1, 2, 3])) # list
+print(type({'name','Aditya'})) # dictionary
+print(type({1.2, 3.14, 2.1})) # set
+print(type((1.2, 3.14, 2.1))) # tuple
