a56c939651326c682c0395add9cfb7ec8634e0ea
Aditya Danturthi <aditya2096@gmail.com>
2022-07-19 22:37:54 -0230

    in-built functions and variables

--- Day2.py
+++ Day2.py
@@ -1,4 +1,4 @@
-# Built-in functions
+# Built-in functions and variables
 
 print("Hello, world!") # prints the string between the quotes
 len('Hello, world!') # prints the length of the string
@@ -7,3 +7,39 @@ str(10) # converts the number into a string
 int('10') # converts the string into int
 float(10) # converts int into float
 input('Enter your name') #accepts input
+
+min(10,20,30,40,50) # returns the minimum value
+max(10,20,30,40,50) # returns the maximum value
+min([10,20,30,40,50]) # takes list as an argument and returns the minimum value
+max([10,20,30,40,50]) # takes list as an argument and returns the maximum value
+sum([10,20,30,40,50]) # takes list as an argument and returns the sum
+
+
+# Variables
+
+first_name = 'Aditya'
+last_name = 'Danturthi'
+country = 'Canada'
+age = 260
+is_married = False
+skills = ['HTML', 'CSS', 'JS', 'SQL', 'Python']
+person_info = {
+   'firstname':'Aditya',
+   'lastname':'Danturthi',
+   'country':'Canada',
+   }
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+# Declaring multiple variables on a single line
+first_name, last_name, country, age, is_married = 'Aditya', 'Danturthi', 'Canada', 260, False
