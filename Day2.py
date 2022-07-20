25bbd8c8b4b8eb9f0781e7470f880dfc67370ab6
Aditya Danturthi <aditya2096@gmail.com>
2022-07-19 22:26:03 -0230

    in-built functions

---
+++ Day2.py
@@ -0,0 +1,9 @@
+# Built-in functions
+
+print("Hello, world!") # prints the string between the quotes
+len('Hello, world!') # prints the length of the string
+type('Hello, world!') # prints the datatype
+str(10) # converts the number into a string
+int('10') # converts the string into int
+float(10) # converts int into float
+input('Enter your name') #accepts input
