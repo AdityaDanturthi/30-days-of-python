f66494a61568d6700d6ad90f8cc388221b1e5d75
Aditya Danturthi <aditya2096@gmail.com>
2022-07-20 19:18:44 -0230

    Assignment and arithmetic operators

--- Day3.py
+++ Day3.py
@@ -4,4 +4,21 @@ print(False)
 
 # Types of operators
 
-#Assignment operators
+# Assignment operators
+x = 5
+x +=3
+x -= 3
+x *= 3
+x /= 3
+x %= 3
+
+
+# Arithmetic operators
+print('Addition: ', 1+2)                     # 3
+print('Subtraction: ', 2-1)                  # 1
+print('Multiplication: ', 2*3)               # 6
+print('Division: ', 4/2)                     # 2.0 python gives floating by default
+print('Division without remainder: ', 7//2)  # 3
+print('Modulus: ', 3%2)                      # 1 gives the remainder
+print('Exponentiation: ', 3**2)              # 9 (3*3)
+
