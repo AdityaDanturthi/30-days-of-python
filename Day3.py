64648673145528f0e82691e958163fc6684e0988
Aditya Danturthi <aditya2096@gmail.com>
2022-07-21 21:47:12 -0230

    Day 3 exercises

--- Day3.py
+++ Day3.py
@@ -22,3 +22,13 @@ print('Division without remainder: ', 7//2)  # 3
 print('Modulus: ', 3%2)                      # 1 gives the remainder
 print('Exponentiation: ', 3**2)              # 9 (3*3)
 
+
+
+#Exercises
+age = 260
+height = 6.0
+complex = 1+3j
+base = input('Please input base of the triangle: ')
+heightoftriangle = input('Please input height of the triangle: ')
+print('Area of triangle: ', 0.5*base*heightoftriangle)
+
