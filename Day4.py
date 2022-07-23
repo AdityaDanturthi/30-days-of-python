f430ae0ee9a864adb6c52c021bc717db798e76a5
Aditya Danturthi <aditya2096@gmail.com>
2022-07-22 23:15:49 -0230

    multiline strings

--- Day4.py
+++ Day4.py
@@ -2,3 +2,21 @@
 letter ='p' # string variable
 print(letter) # p
 print(len(letter)) # 1
+greeting = 'Hello, World!'
+print(greeting) # Hello, World!
+print(len(greeting)) # 13
+sentence = 'I am enjoying 30 days of python'
+print(sentence)
+print(len(sentence)) # 31
+
+# Declaring multiline strings
+multiline_string = '''I am a programmer and enjoy programming.
+It is rewarding to do what you are passionate about.
+That is why I am doing 30 days of python'''
+print(multiline_string)
+
+# Multiline string using double quotes
+multiline_string = """I am a programmer and enjoy programming.
+It is rewarding to do what you are passionate about.
+That is why I am doing 30 days of python"""
+print(multiline_string)
