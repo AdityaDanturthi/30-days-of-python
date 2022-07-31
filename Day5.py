130b06e4e28ccf1eed1d2b2eb343bb61681a4984
Aditya Danturthi <aditya2096@gmail.com>
2022-07-30 22:00:52 -0230

    checking, adding, inserting, removing, clearing and modifying a list

--- Day5.py
+++ Day5.py
@@ -31,3 +31,35 @@ item1, item2, itmem3, item4 = fruits
 # Slicing items in a list
 print(fruits[0:4]) # returns all fruits
 print(fruits[0:]) # returns all fruits
+print(fruits[1:3]) # excludes the first index
+
+# Modifying a list
+fruits[1] = 'pineapple'
+
+# Checking if an item is in a list
+doesitexist = 'pineapple' in fruits
+
+# Adding items in a list
+fruits.append('mango')
+
+# Inserting items in a list
+lst = ['item1','item2']
+lst.insert(0,'item') # adds item in the beginning of the list
+
+# Removing items from a list
+lst.remove('item')
+
+# removing items using index
+lst.pop(0)
+
+del lst[0] # same as .pop()
+
+# Clearing a list
+lst = ['item1','item2']
+lst.clear() # removes all items
+
+# Copying a list
+fruits_copy = fruits.copy()
+
+
+
