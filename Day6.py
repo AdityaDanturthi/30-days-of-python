4e48084a9f370a7e799a6f27301a63a8502a07f5
Aditya Danturthi <aditya2096@gmail.com>
2022-08-03 08:21:40 -0230

    Creating a tuple and length of tuple

--- Day6.py
+++ Day6.py
@@ -1,2 +1,9 @@
+# Tuples
+# Tuples are written with round brackets ()
+
 # Creating a tuple
 empty_tuple = tuple()
+
+
+fruits = ('apple','banana', 'orange', 'mango')
+print(len(fruits)) # lenght of tuple
