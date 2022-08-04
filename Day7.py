43c84f228ec2abad2ec19677823a694746c05a18
Aditya Danturthi <aditya2096@gmail.com>
2022-08-04 08:24:28 -0230

    Checking an item in a set and accessing items in a set using a loop

--- Day7.py
+++ Day7.py
@@ -1,9 +1,22 @@
 # Sets: Collection of unordered and unindexed distinct elements
 
 # Creating a set
+from operator import le
+
+
 st = {}
 # or 
 st = set()
 
 # Example
 fruits = {'banana','apple','orange', 'pineapple'}
+
+# Length of a set
+print(len(fruits))
+
+# Accessing items in a set (Can only be accessed through loops)
+for fruit in fruits:
+    print('Fruits in the set: %s' %(fruit))
+
+# Checking an item in a set
+print('apple' in fruits) # True
