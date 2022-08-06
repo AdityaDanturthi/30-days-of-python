f38a756691e7ebb494e351e7f3dce07470c176b7
Aditya Danturthi <aditya2096@gmail.com>
2022-08-06 20:08:38 -0230

    creating a dictionary and checking length

---
+++ Day8.py
@@ -0,0 +1,11 @@
+# Dictionaries
+
+# Creating a dictionary
+emptyDict = {}
+
+dct = {'Name':'Aditya Danturthi',
+        'Age': 260
+}
+
+# Dictionary length
+print(len(dct)) # Prints number of key value pairs
