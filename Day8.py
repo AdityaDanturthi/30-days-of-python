e590624895a0c3ddd846078cacf2c867203d2ae6
Aditya Danturthi <aditya2096@gmail.com>
2022-08-06 20:18:14 -0230

    adding and modifying items

--- Day8.py
+++ Day8.py
@@ -19,3 +19,10 @@ print(len(dct)) # Prints number of key value pairs
 print(dct['Name']) # Aditya Danturthi
 print(dct['skills'][4]) # Python
 print(dct['address']['zipcode']) # A1BC2D
+
+# Adding items to a dictionary
+dct['skills'].append('scikit-learn')
+
+# Modifying items in a dictionary
+dct['age'] = 262
+
