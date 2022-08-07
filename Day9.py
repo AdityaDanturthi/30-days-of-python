cf2bfb841d779b3fe43ae416f90adfea1eae6e19
Aditya Danturthi <aditya2096@gmail.com>
2022-08-07 10:59:55 -0230

    if-else

--- Day9.py
+++ Day9.py
@@ -10,3 +10,11 @@
 a = 3
 if a > 2:
     print('a is greater than 2')
+
+# if-else
+if a > 3:
+    print('a is greater than 3')
+elif a ==3:
+    print('a is equal to 3')
+elif a < 3:
+    print('a is less than 3')
