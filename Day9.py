de1817e3c760a7e0c8085d7da47da969b0e98a2e
Aditya Danturthi <aditya2096@gmail.com>
2022-08-07 11:11:20 -0230

    nested if

--- Day9.py
+++ Day9.py
@@ -18,3 +18,20 @@ elif a ==3:
     print('a is equal to 3')
 elif a < 3:
     print('a is less than 3')
+
+# Short hand
+# code if condition else
+a = 3
+print('a is greater than 2') if a > 2 else print('a is less than 2')
+
+# Nested if
+b = 0
+if b > 0:
+    if b%2 == 0:
+        print('b is a positive and even number')
+    else:
+        print('b is a positive number')
+elif b == 0:
+    print('b is 0')
+else:
+    print('b is a negative number')
