d2f4e61d3c127979f5ce072fccfbf488171f8d89
Aditya Danturthi <aditya2096@gmail.com>
2022-08-08 08:51:25 -0230

    break and continue in while loop

--- Day10.py
+++ Day10.py
@@ -7,3 +7,20 @@ while count < 5:
     count += 1
 else:
     print(count)
+
+# break and continue
+count = 0
+while count < 5:
+    print(count) # print 0, 1, 2
+    count += 1
+    if count == 3:
+        break
+
+count = 0
+while count < 5:
+    print(count) # print 0, 1, 2, 4 (skips 3)
+    count += 1
+    if count == 3:
+        continue
+    print(count)
+    count += 1
