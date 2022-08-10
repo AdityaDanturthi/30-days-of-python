44171616af6bc8e0800e5cd0a8a5ebcf3d20e1f8
Aditya Danturthi <aditya2096@gmail.com>
2022-08-10 08:21:46 -0230

    Defining a function with and without returning function

--- Day11.py
+++ Day11.py
@@ -4,11 +4,20 @@
 def fullname():
     firstname = 'Aditya'
     lastname = 'Danturthi'
+    name = firstname+' '+lastname
+    print(name)
+fullname()
+
+def fullnamereturn():
+    firstname = 'Aditya'
+    lastname = 'Danturthi'
     return firstname+' '+lastname
-print(fullname())
+print(fullnamereturn())
 
 def addtwonumbers():
     num1 = 1
     num2 = 2
     return num1 + num2
 print(addtwonumbers())
+
+
