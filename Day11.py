575ea7d3a76e1659c2cb21b5437d551dab7bfb34
Aditya Danturthi <aditya2096@gmail.com>
2022-08-10 08:19:02 -0230

    Defining a function without parameters

---
+++ Day11.py
@@ -0,0 +1,14 @@
+# Functions
+
+# Declaring a function without parameters
+def fullname():
+    firstname = 'Aditya'
+    lastname = 'Danturthi'
+    return firstname+' '+lastname
+print(fullname())
+
+def addtwonumbers():
+    num1 = 1
+    num2 = 2
+    return num1 + num2
+print(addtwonumbers)
