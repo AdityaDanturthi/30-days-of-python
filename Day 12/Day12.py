ba49f59adc3d05f0812fd77e81989a614f44cefb
Aditya Danturthi <aditya2096@gmail.com>
2022-08-11 15:15:40 -0230

    Module creation

--- Day 12/Day12.py
+++ Day 12/Day12.py
@@ -1,1 +1,5 @@
+# Modules: A module is a file containing a set of codes or a set of functions which can be included in an application. A module could be a file containing a single variable, a function or a big code base.
 
+# Creating a module: fullnamefirstmodule.py
+import fullnamefirstmodule
+print(fullnamefirstmodule.fullname('Aditya', 'Danturthi'))
