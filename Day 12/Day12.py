3b9f4656bf433b9f01b9eb27c567ef381d154236
Aditya Danturthi <aditya2096@gmail.com>
2022-08-11 15:23:31 -0230

    Imported created modules and passed parameters

--- Day 12/Day12.py
+++ Day 12/Day12.py
@@ -1,5 +1,6 @@
 # Modules: A module is a file containing a set of codes or a set of functions which can be included in an application. A module could be a file containing a single variable, a function or a big code base.
 
 # Creating a module: fullnamefirstmodule.py
-import fullnamefirstmodule
+import fullnamefirstmodule 
 print(fullnamefirstmodule.fullname('Aditya', 'Danturthi'))
+print(fullnamefirstmodule.addtwonumbers(1,2,3,4,5,6,7,8,9,10))
