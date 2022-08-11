bbbbc1b10ccf47821a58632dba9ab2e4da77f9d6
Aditya Danturthi <aditya2096@gmail.com>
2022-08-11 15:24:04 -0230

    added "sum of numbers" function

--- Day 12/fullnamefirstmodule.py
+++ Day 12/fullnamefirstmodule.py
@@ -1,3 +1,9 @@
 def fullname(firstname, lastname):
     name = firstname+' '+lastname
     return name
+
+def addtwonumbers(*nums):
+    total =0
+    for i in nums:
+        total += i
+    return total
