f3b6e9fba32f7b363bf51f427c3218ece0c3a9b4
Aditya Danturthi <aditya2096@gmail.com>
2022-08-14 20:49:28 -0230

    Function as a return value

--- Day14.py
+++ Day14.py
@@ -10,3 +10,33 @@ def higherorderfunction(f, lst): # function as a parameter
 
 result = higherorderfunction(addnums, [1, 2, 3, 4, 5])
 print(result)
+
+# Function as a return value
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def absolute(x):
+    if x >= 0:
+        return x
+    else:
+        return -(x)
+    
+def hof(type):
+    if type == 'square':
+        return square
+    elif type == 'cube':
+        return cube
+    elif type == 'absolute':
+        return absolute
+    
+res = hof('square')
+print(res(2))
+
+res = hof('cube')
+print(res(2))
+
+res = hof('absolute')
+print(res(-2))
