6fd5bf91ae919e7feaaca7530954e6c5ead89dc3
Aditya Danturthi <aditya2096@gmail.com>
2022-08-16 11:31:39 -0230

    Types of errors
    syntax, name, index, modulenotfound and attribute errors

---
+++ Day15.py
@@ -0,0 +1,22 @@
+# Type of errors
+
+# SyntaxError
+print 'Hello world' # SyntaxError: missing parantheses
+
+# NameError
+print(a) # NameError: a is not defined
+
+# IndexError
+nums = [1, 2, 3, 4, 5]
+print(nums[5]) # IndexError: list index out of range
+
+# ModuleNotFoundError
+import maths # ModuleNotFoundError: No module named 'maths'
+
+# AttributeError
+import math
+math.PI # AttributeError: module math has no attribut 'PI'
+
+
+
+
