3736924dc06301cec92ecd23f191c02f27eaa248
Aditya Danturthi <aditya2096@gmail.com>
2022-08-16 11:40:57 -0230

    Types of errors
    Key, type, import,  value and ZeroDivision errors

--- Day15.py
+++ Day15.py
@@ -17,6 +17,22 @@ import maths # ModuleNotFoundError: No module named 'maths'
 import math
 math.PI # AttributeError: module math has no attribut 'PI'
 
+# KeyError
+details = {'name': 'Aditya', 'age': 260}
+details['name']
+details['country'] # KeyError: 'country'
+
+# TypeError
+1 + '2' # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+# ImportError
+from math import power # ImportError: cannot import name 'power' from 'math'
+
+# ValueError
+int('12a') # ValueError: invalid literal for int() with base 10: '12a'
+
+# ZeroDivisionError
+1/0 # ZeroDivisionError: division by zero
 
 
 
