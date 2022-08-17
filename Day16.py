a68555a900b336e09221a70c23de9c84ec8ad1db
Aditya Danturthi <aditya2096@gmail.com>
2022-08-17 08:57:23 -0230

    Formatting date output using strftime

--- Day16.py
+++ Day16.py
@@ -2,19 +2,49 @@
 
 import datetime
 from sqlite3 import Timestamp
-from time import time
+from time import strftime, time
 print(dir(datetime))
 
 # Getting date time information
 from datetime import datetime
-now = datetime.now()
-day = now.day
-month = now.month
-year = now.year
-hour = now.hour
-minute = now.minute
-second = now.second
-timestamp = now.timestamp()
+now1 = datetime.now()
+day = now1.day
+month = now1.month
+year = now1.year
+hour = now1.hour
+minute = now1.minute
+second = now1.second
+timestamp = now1.timestamp()
 
 print(day, month, year, hour, minute, second)
 print('Timestamp: ', timestamp)
+
+print(f'{day}/{month}/{year}, {hour}:{minute}')
+
+# New Year
+
+from datetime import datetime
+newyear = datetime(2022,1,1)
+print(newyear)
+day = newyear.day
+month = newyear.month
+year = newyear.year
+hour = newyear.hour
+minute = newyear.minute
+second = newyear.second
+
+print(day, month, year, hour, minute, second)
+print(f'{day}/{month}/{year}, {hour}:{minute}')
+
+# Formatting date output using strftime
+
+from datetime import *
+now2 = datetime.now()
+t = now2.strftime('%H:%M:%S')
+print('Time: ',t)
+
+t1 = now2.strftime("%m-%d-%Y, %H:%M %p")
+print(t1)
+
+t2 = now2.strftime("%d-%m-%Y, %H:%M %p")
+print(t2)
