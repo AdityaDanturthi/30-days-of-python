1b195e8032906dc04b06d414c7e7ed35a9ac82e2
Aditya Danturthi <aditya2096@gmail.com>
2022-08-17 15:50:06 -0230

    Using date from datetime and time objects

--- Day16.py
+++ Day16.py
@@ -56,3 +56,30 @@ print('Date string: ',datestr)
 dateobj = datetime.strptime(datestr, '%d %B, %Y')
 
 print('Date object: ',dateobj)
+
+#  Using date from datetime
+d = date(2022,1,1)
+print(d)
+
+print('Current date:', d.today())
+
+# Date object of today's date
+today = date.today()
+print('Current year: ', today.year)
+print('Current month: ', today.month)
+print('Current day: ', today.day)
+
+# Time objects to represent time
+from datetime import time
+
+a = time()
+print('Time:',a)
+
+b = time(10,30,50) # time(hours, minutes, seconds)
+print(b)
+
+c = time(hour = 10, minute = 50, second = 5)
+print(c)
+
+d = time(10,50,5, 100000) # last parameter is microseconds
+print(d)
