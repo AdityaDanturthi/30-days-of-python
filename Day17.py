bed7c23a687f51d69a29cbe7949f70bb8b8c59de
Aditya Danturthi <aditya2096@gmail.com>
2022-08-19 10:52:19 -0230

    Unpacking a dictionary

--- Day17.py
+++ Day17.py
@@ -1,38 +1,38 @@
 # Exception handling
-from datetime import datetime
+# # from datetime import datetime
 
-try:
-    print(10 + '2')
-except:
-    print(' Something went wrong')
+# # try:
+# #     print(10 + '2')
+# # except:
+# #     print(' Something went wrong')
 
-# Kind of error
-try:
-    name = input('Please input your name: ')
-    birthyear = int(input('Please input your birth year: '))
-    dateobj = datetime.now()
-    thisyear = dateobj.year
-    thisyear = int(thisyear)
-    age = thisyear - birthyear
-    print(f'Hi {name}! You are {age} years old.')
-except TypeError:
-    print('Type error occured')
-except ValueError:
-    print('Value error occured')
-except ZeroDivisionError:
-    print('zero division error occured')
+# # # Kind of error
+# # try:
+# #     name = input('Please input your name: ')
+# #     birthyear = int(input('Please input your birth year: '))
+# #     dateobj = datetime.now()
+# #     thisyear = dateobj.year
+# #     thisyear = int(thisyear)
+# #     age = thisyear - birthyear
+# #     print(f'Hi {name}! You are {age} years old.')
+# # except TypeError:
+# #     print('Type error occured')
+# # except ValueError:
+# #     print('Value error occured')
+# # except ZeroDivisionError:
+# #     print('zero division error occured')
 
-# Shortening the above code
-try:
-    name = input('Please input your name: ')
-    birthyear = int(input('Please input your birth year: '))
-    dateobj = datetime.now()
-    thisyear = dateobj.year
-    thisyear = int(thisyear)
-    age = thisyear - birthyear
-    print(f'Hi {name}! You are {age} years old.')
-except Exception as e:
-    print(e)
+# # # Shortening the above code
+# # try:
+# #     name = input('Please input your name: ')
+# #     birthyear = int(input('Please input your birth year: '))
+# #     dateobj = datetime.now()
+# #     thisyear = dateobj.year
+# #     thisyear = int(thisyear)
+# #     age = thisyear - birthyear
+# #     print(f'Hi {name}! You are {age} years old.')
+# # except Exception as e:
+# #     print(e)
 
 # Packing and unpacking arguments in python
 # We use two operators:
@@ -64,3 +64,9 @@ print(us, rest)
 one, *restnums= lst
 print(one, restnums)
 
+# Unpacking dictionaries
+def unpackingdetails(name, country, age):
+    return f'{name} lives in {country}. He is {age} years old'
+
+dct = { 'name': 'Aditya', 'country': 'Canada', 'age': 260}
+print(unpackingdetails(**dct))
