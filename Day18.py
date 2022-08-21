873fe7f4cf6ea28175730ac526d844c09442b996
Aditya <aditya2096@gmail.com>
2022-08-20 22:38:54 -0230

    regular expressions
    square bracket in regex variable and 'or' condition

--- Day18.py
+++ Day18.py
@@ -76,3 +76,11 @@ regexapplebothcases = r'[Aa]pple'
 matchescaseinsensitive1 = re.findall(regexapplebothcases, txtfruits)
 print(matchescaseinsensitive1)
 
+# Square bracket
+fruitsstr = 'Apple and banana are fruits. An old cliche says an apple a day a doctor way has been replaced by a banana a day keeps the doctor far far away.'
+applematches = re.findall(regexapplebothcases, fruitsstr)
+print(applematches)
+
+regexappleandbanana = r'[Aa]pple | [Bb]anana'
+appleandbananamatches = re.findall(regexappleandbanana, fruitsstr)
+print(appleandbananamatches)
