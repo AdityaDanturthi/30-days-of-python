83da224d2fa3fdc85add7fe52ba4b74e10d01149
Aditya <aditya2096@gmail.com>
2022-08-21 22:02:27 -0230

    file handling
    readline() and readlines()

--- Day19.py
+++ Day19.py
@@ -4,4 +4,20 @@
 f = open('example.txt')
 text = f.read()
 print(text)
+
+
+# Limiting print to first 10 characters
+text10 = f.read(10)
+print(type(text10))
+print(text10)
+
+# readline(): reads only the first one
+line = f.readline()
+print(type(line))
+print(line)
+
+# readlines(): read all the text line by line and returns list of lines
+lines = f.readlines()
+print(type(lines))
+print(lines)
 f.close()
