68f0696bce2ed295f1ac9138878fd640af7922b0
Aditya <aditya2096@gmail.com>
2022-08-23 09:55:30 -0230

    File handling
    Converting JSON to a dictionary

--- Day19.py
+++ Day19.py
@@ -1,62 +1,62 @@
-# # # # File Handling
+# File Handling
 
-# # # # Opening files for reading
-# # # f = open('example.txt')
-# # # text = f.read()
-# # # print(text)
+# Opening files for reading
+f = open('example.txt')
+text = f.read()
+print(text)
 
 
-# # # # Limiting print to first 10 characters
-# # # text10 = f.read(10)
-# # # print(type(text10))
-# # # print(text10)
+# Limiting print to first 10 characters
+text10 = f.read(10)
+print(type(text10))
+print(text10)
 
-# # # # readline(): reads only the first one
-# # # line = f.readline()
-# # # print(type(line))
-# # # print(line)
+# readline(): reads only the first one
+line = f.readline()
+print(type(line))
+print(line)
 
-# # # # readlines(): read all the text line by line and returns list of lines
-# # # lines = f.readlines()
-# # # print(type(lines))
-# # # print(lines)
-# # # f.close()
+# readlines(): read all the text line by line and returns list of lines
+lines = f.readlines()
+print(type(lines))
+print(lines)
+f.close()
 
-# # # # splitlines(): another way to get all the lines
-# # # splitlines = f.read().splitlines()
-# # # print(type(splitlines))
-# # # print(splitlines)
-# # # f.close()
+# splitlines(): another way to get all the lines
+splitlines = f.read().splitlines()
+print(type(splitlines))
+print(splitlines)
+f.close()
 
-# # # # Closing a file automatically
-# # # with open('example.txt') as f:
-# # #     splitlines = f.read().splitlines()
-# # #     print(type(splitlines))
-# # #     print(splitlines)
+# Closing a file automatically
+with open('example.txt') as f:
+    splitlines = f.read().splitlines()
+    print(type(splitlines))
+    print(splitlines)
 
-# # # # Opening files for writing and appending
-# # # with open('example.txt', 'a') as f:
-# # #     f.write('This text has to be appended at the end.')
+# Opening files for writing and appending
+with open('example.txt', 'a') as f:
+    f.write('This text has to be appended at the end.')
 
-# # # with open('example.txt') as f:
-# # #     lines1 = f.read().splitlines()
-# # #     print(lines1)
+with open('example.txt') as f:
+    lines1 = f.read().splitlines()
+    print(lines1)
 
-# # # # 'w' creates a new file, if it doesn't exist
-# # # with open('writingfileexample.txt', 'w') as f:
-# # #     f.write('This will be written in a new file!')
+# 'w' creates a new file, if it doesn't exist
+with open('writingfileexample.txt', 'w') as f:
+    f.write('This will be written in a new file!')
 
-# # # with open('writingfileexample.txt') as f:
-# # #     newfile = f.read().splitlines()
-# # #     print(newfile)
+with open('writingfileexample.txt') as f:
+    newfile = f.read().splitlines()
+    print(newfile)
 
-# # # # Deleting a file
-# # # import os
-# # # if os.path.exists('p:/c/writingfileexample.txt'):
-# # #     os.remove('p:/c/writingfileexample.txt')
-# # #     print('Sucessfully deleted!')
-# # # else:
-# # #     print('File does not exist!')
+# Deleting a file
+import os
+if os.path.exists('p:/c/writingfileexample.txt'):
+    os.remove('p:/c/writingfileexample.txt')
+    print('Sucessfully deleted!')
+else:
+    print('File does not exist!')
 
 # File types
 # Files with JSON extension: JSON very similar to python dictionary except it is written on a single line
