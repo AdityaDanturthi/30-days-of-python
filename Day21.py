0a2688ea5797beccab13a1eeb0f7ff800883ce8e
Aditya <aditya2096@gmail.com>
2022-08-26 08:42:18 -0230

    classes and objects
    class constructor and adding more parameters

--- Day21.py
+++ Day21.py
@@ -8,3 +8,25 @@ print(Person)
 # Creating an object
 p = Person()
 print(p)
+
+# Class constructor
+class Person:
+    def __init__(self, name): # self allows to add a parameter to a class
+        self.name = name
+
+p = Person('Aditya')
+print(p.name)
+print(p)
+
+# Adding more parameters to the constructor
+class Person:
+    def __init__(self, name, province, age): # self allows to add a parameter to a class
+        self.name = name
+        self.province = province
+        self.age = age
+
+p = Person('Aditya', 'NL', 260)
+print(f' Hi %s from %s of age %d' %(p.name,p.province,p.age))
+print(p)
+
+
