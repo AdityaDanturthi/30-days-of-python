d548588a084bdcbd27bf5eec316492c4a9666317
Aditya <aditya2096@gmail.com>
2022-08-26 09:38:39 -0230

    classes and objects
    Default values during instantiation to avoid errors when object is called without passing parameters

--- Day21.py
+++ Day21.py
@@ -41,3 +41,16 @@ class Person:
 p = Person('Aditya','NL',260)
 print(p.details())
 
+# Default values during instantiation to avoid errors when object is called without passing parameters
+class Person:
+    def __init__(self, name = 'Aditya', province = 'NL', age = 260):
+        self.name = name
+        self.province = province
+        self.age = age
+    def details(self):
+        return f'Hi {self.name} from {self.province} of age {self.age}'
+
+p1 = Person()
+print(p1.details())
+p2 = Person('Alex', 'QC', 260)
+print(p2.details())
