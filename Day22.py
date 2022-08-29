9169b4b134143a8adb0db90b7381b6beca413ce5
Aditya <aditya2096@gmail.com>
2022-08-29 08:46:53 -0230

    web scraping
    beautifulsoup

--- Day22.py
+++ Day22.py
@@ -1,4 +1,4 @@
-# Python Web Scraping
+# Web Scraping
 import requests
 from bs4 import beautifulsoup
 
@@ -7,3 +7,17 @@ url = 'https://archive.ics.uci.edu/ml/datasets.php'
 response = requests.get(url)
 status = response.status_code
 print(status)
+
+# Using BeautifulSoup to parse content from the page
+content = response.content
+soup = beautifulsoup(content, 'html.parser')
+print(soup.title)
+print(soup.title.get_text())
+print(soup.body)
+print(soup.status_code)
+
+tables = soup.find_all('table', {'cellpadding': '3'})
+
+table = tables[0]
+for td in table.find('tr').find_all('td'):
+    print(td.text)
