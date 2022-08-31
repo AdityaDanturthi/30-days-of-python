c8b0a57db02b23570cc5674f3adef04aecfe69ee
Aditya <aditya2096@gmail.com>
2022-08-31 13:32:45 -0230

    statistical analysis
    Creating boolean numpy array from a list
    Creating a multidimensional array using numpy
    Converting a numpy array to a list

--- Day24.py
+++ Day24.py
@@ -23,3 +23,19 @@ print(nparray)
 nparrayfloat = np.array(pylist, dtype = float)
 print('Type: ', type(nparrayfloat))
 print(nparrayfloat)
+
+# Creating boolean numpy array from a list
+listforbool = [0,1,1,0,1,0, -1]
+nparraybool = np.array(listforbool, dtype= bool)
+print('Type: ', type(nparraybool))
+print(nparraybool)
+
+# Creating a multidimensional array using numpy
+twodimlstnparrray = np.array(twodimlst)
+print(type(twodimlstnparrray))
+print(twodimlstnparrray)
+
+# Converting a numpy array to a list
+nparratolist = nparray.tolist()
+print(type(nparratolist))
+print(nparratolist)
