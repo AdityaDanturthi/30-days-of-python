8d4834312c86c7d9dc7a66129e1e0e9ccc6b86da
Aditya <aditya2096@gmail.com>
2022-08-31 10:53:16 -0230

    statistical analysis
    creating a python list and a numpy arrray from it

--- Day24.py
+++ Day24.py
@@ -4,3 +4,18 @@
 import numpy as np
 print('numpy version: ', np.__version__)
 print(dir(np)) # prints available functions
+
+# Creating a python list
+pylist  = [1,2,3,4,5]
+
+print('Type: ',type(pylist))
+print(pylist)
+
+twodimlst = [[1,2],[3,4],[5,6],[7,8],[9,10]]
+print('Two dimensional list: ', twodimlst)
+
+# Creating a numpy array from python list
+nparray = np.array(pylist)
+print('Type: ', type(nparray))
+print(nparray)
+
