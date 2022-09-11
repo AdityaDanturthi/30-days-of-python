6196bf745b7a8abc6e3a768f6072a52fe9cc28b0
Aditya <aditya2096@gmail.com>
2022-09-11 16:33:00 -0230

    flask

--- Day 26/app.py
+++ Day 26/app.py
@@ -1,1 +1,21 @@
+# Panadas for web
+
+# Creating routes
+
+# Home route
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def home ():
+    return '<h1>Hi</h1>'
+
+@app.route('/about')
+def about ():
+    return '<h1>About me</h1>'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("Port", 5000))
+    app.run(debug = True, host = '0.0.0.0', port = port)
 
