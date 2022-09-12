7c7be14fc1b7310bd782705b518a636515010f1a
Aditya <aditya2096@gmail.com>
2022-09-12 15:55:12 -0230

    flask

--- Day 26/app.py
+++ Day 26/app.py
@@ -1,21 +1,28 @@
-# Panadas for web
+# Python for web
 
 # Creating routes
 
 # Home route
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 import os
 app = Flask(__name__)
 
 @app.route('/')
-def home ():
-    return '<h1>Hi</h1>'
+def home():
+    tech = ['HTML', 'CSS', 'Flask', 'Python']
+    name = '30 Days of Python'
+    return render_template('home.html', tech = tech, name = name, title = 'Home')
 
 @app.route('/about')
-def about ():
-    return '<h1>About me</h1>'
+def about():
+    name = '30 Days of Python'
+    return render_template('about.html', name = name, title = 'About Me')
+
+@app.route('/post')
+def post():
+    name = 'Text Analyzer'
+    return render_template('post.html', name = name, title = name)
 
 if __name__ == '__main__':
     port = int(os.environ.get("Port", 5000))
     app.run(debug = True, host = '0.0.0.0', port = port)
-
