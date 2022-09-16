95be80d1f824b22b4c0e09eb86db83daaadb3310
Aditya <aditya2096@gmail.com>
2022-09-16 08:58:54 -0230

    Add files via upload

--- Day 26/app.py
+++ Day 26/app.py
@@ -1,28 +1,40 @@
 # Python for web
 
-# Creating routes
-
-# Home route
 from flask import Flask, render_template, request, redirect, url_for
-import os
-app = Flask(__name__)
+import os # importing operating system module
 
-@app.route('/')
-def home():
-    tech = ['HTML', 'CSS', 'Flask', 'Python']
-    name = '30 Days of Python'
-    return render_template('home.html', tech = tech, name = name, title = 'Home')
+app = Flask(__name__)
+# to stop caching static file
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+
+@app.route('/') # this decorator create the home route
+def home ():
+    techs = ['HTML', 'CSS', 'Flask', 'Python']
+    name = '30 Days Of Python Programming'
+    return render_template('home.html', techs=techs, name = name, title = 'Home')
 
 @app.route('/about')
 def about():
-    name = '30 Days of Python'
-    return render_template('about.html', name = name, title = 'About Me')
+    name = '30 Days Of Python Programming'
+    return render_template('about.html', name = name, title = 'About Us')
 
-@app.route('/post')
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/post', methods= ['GET','POST'])
 def post():
     name = 'Text Analyzer'
-    return render_template('post.html', name = name, title = name)
+    if request.method == 'GET':
+         return render_template('post.html', name = name, title = name)
+    if request.method =='POST':
+        content = request.form['content']
+        print(content)
+        return redirect(url_for('result'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("Port", 5000))
-    app.run(debug = True, host = '0.0.0.0', port = port)
+    # for deployment
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
