194d93046f55b3bc391d016eb73ab3ced643c698
Aditya Danturthi <aditya2096@gmail.com>
2022-11-23 15:17:30 -0330

    exercises

--- Day10.py
+++ Day10.py
@@ -202,6 +202,26 @@ for i in range(0,100,2):
 
 # Use for loop to iterate from 0 to 100 and print only odd numbers
 print("Odd numbers between 0 and 100:")
-for i in range(0,100,1):
+for i in range(1,101,2):
     print(i)
     
+# Exercises: Level 2
+
+# Use for loop to iterate from 0 to 100 and print the sum of all numbers. The sum of all numbers is 5050.
+sum = 0
+for i in range(0,101):
+    sum += i
+print("Sum of all numbers from 0 to 100 is:", sum)
+
+
+# Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. The sum of all evens is 2550. And the sum of all odds is 2500.
+evensum = 0
+for i in range(0,101, 2):
+    evensum += i
+print("Sum of all even numbers from 0 to 100 is:", evensum)
+
+oddsum = 0
+for i in range(1,101, 2):
+    oddsum += i
+print("Sum of all odd numbers from 0 to 100 is:", oddsum)
+
