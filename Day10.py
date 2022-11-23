11ebc0a10f947c54cdd14b060479de5382035ea8
Aditya Danturthi <aditya2096@gmail.com>
2022-11-23 14:29:14 -0330

    exercises

--- Day10.py
+++ Day10.py
@@ -169,3 +169,29 @@ for i in range(8):
     for j in range(8):
         print("#", end = ' ')
     print(" ")
+
+"""Print the following pattern:
+
+0 x 0 = 0
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
+4 x 4 = 16
+5 x 5 = 25
+6 x 6 = 36
+7 x 7 = 49
+8 x 8 = 64
+9 x 9 = 81
+10 x 10 = 100"""
+
+for i in range(11):
+        temp = i*i
+        print(f"{i} x {i} = {temp}")
+print()
+
+
+# Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
+
+langlist = ['Python', 'Numpy','Pandas','Django', 'Flask']
+for l in langlist:
+    print(l)
